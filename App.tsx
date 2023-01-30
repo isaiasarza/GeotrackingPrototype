73a3c76840584de6ced990518493b7d16e291639
isaiasarza <arza.isaias@gmail.com>
2023-01-30 19:42:19 -0300

    setting maps directions api

--- App.tsx
+++ App.tsx
@@ -10,7 +10,13 @@ import {StyleSheet, View} from 'react-native';
 
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
+import Config from 'react-native-config';
+import MapViewDirections from 'react-native-maps-directions';
+
 function App(): JSX.Element {
+  const origin = {latitude: -42.7692, longitude: -65.03851};
+  const destination = {latitude: -42.773038, longitude: -65.05112};
+  const GOOGLE_MAPS_API_KEY = Config.GOOGLE_MAPS_API_KEY ?? ''; //
   return (
     <View style={styles.container}>
       <MapView
@@ -24,12 +30,17 @@ function App(): JSX.Element {
           longitudeDelta: 0.0421,
         }}
       >
-        <Marker
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_API_KEY}
+        />
+        {/* <Marker
           key={1}
           coordinate={{latitude: -42.7692, longitude: -65.03851}}
           title={'Marker'}
           description={'Description'}
-        />
+        /> */}
       </MapView>
     </View>
   );
