97c9c8a3d2babd8b242be81b906ca1126debcded
isaiasarza <arza.isaias@gmail.com>
2023-01-31 09:54:23 -0300

    adding waypoints an markers

--- App.tsx
+++ App.tsx
@@ -6,9 +6,9 @@
  */
 
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE, Circle} from 'react-native-maps';
 
 import Config from 'react-native-config';
 import MapViewDirections from 'react-native-maps-directions';
@@ -16,6 +16,11 @@ import MapViewDirections from 'react-native-maps-directions';
 function App(): JSX.Element {
   const origin = {latitude: -42.7692, longitude: -65.03851};
   const destination = {latitude: -42.773038, longitude: -65.05112};
+  const waypoints = [
+    {latitude: -42.784831, longitude: -65.057518},
+    {latitude: -42.777933, longitude: -65.065929},
+  ];
+  const coordinates = [origin, ...waypoints, destination];
   const GOOGLE_MAPS_API_KEY = Config.GOOGLE_MAPS_API_KEY ?? ''; //
   return (
     <View style={styles.container}>
@@ -30,17 +35,18 @@ function App(): JSX.Element {
           longitudeDelta: 0.0421,
         }}
       >
+        {coordinates.map((coordinate, key) => {
+          return (
+            <Marker coordinate={coordinate} pinColor={'#5c881a'} title={'Parada #' + key} description={'Description'}></Marker>
+          );
+        })}
         <MapViewDirections
           origin={origin}
           destination={destination}
+          waypoints={waypoints}
           apikey={GOOGLE_MAPS_API_KEY}
+          strokeColor={'#5c881a'}
         />
-        {/* <Marker
-          key={1}
-          coordinate={{latitude: -42.7692, longitude: -65.03851}}
-          title={'Marker'}
-          description={'Description'}
-        /> */}
       </MapView>
     </View>
   );
